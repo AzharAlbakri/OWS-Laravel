30a7620c10f8571bab984ef208e0eef2a1250bbd
AzharAlbakri <albakriazhar.work@gmail.com>
2019-02-13 23:02:10 +0200

    OWS with laravel

---
+++ lsapp/resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+<div class="jumbotron text-center">
+
+  <img href="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.theconversation.com%2Ffiles%2F232705%2Foriginal%2Ffile-20180820-30593-1nxanpj.jpg%3Fixlib%3Drb-1.1.0%26q%3D45%26auto%3Dformat%26w%3D496%26fit%3Dclip&imgrefurl=http%3A%2F%2Ftheconversation.com%2Fwhy-the-weather-forecast-will-always-be-a-bit-wrong-101547&docid=1OpjznpsZbmp8M&tbnid=URVviH2mkJPSqM%3A&vet=10ahUKEwj0ssHcwrngAhW9DWMBHb_LDGgQMwhqKAIwAg..i&w=496&h=331&bih=674&biw=1536&q=weather&ved=0ahUKEwj0ssHcwrngAhW9DWMBHb_LDGgQMwhqKAIwAg&iact=mrc&uact=8#h=331&imgdii=KdIULuTZ5qA-hM:&vet=10ahUKEwj0ssHcwrngAhW9DWMBHb_LDGgQMwhqKAIwAg..i&w=496" alt="">
+  <h1>Welcome to Laravel</h1>
+  <p>This is laravel application</p>
+  <p><a class="btn btn-primary btn-lg" href="/login" role="button">login</a> <a class="btn btn-success btn-lg" href="/register" role="button">register</a></p> 
+</div>
+  
+@endsection
